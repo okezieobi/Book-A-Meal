0d1938f5bb2a1f1f8151f9f33ce33bd69bf3dd0a
okezieobi <obiedereokezie@gmail.com>
2019-02-15 10:42:47 +0100

    [Chore #163671470] made corrects to index file in models dir

--- models/index.js
+++ models/index.js
@@ -1,10 +1,11 @@
 class BookAMeal {}
 
 class Meal extends BookAMeal {
-  constructor(mealName, mealPrice) {
+  constructor(mealName, mealPrice, mealId) {
     super();
     this.optionList = [];
     this.optionFormat = {
+      id: mealId,
       name: mealName,
       price: mealPrice,
     };
@@ -25,10 +26,11 @@ class Menu extends Meal {
 }
 
 class Order extends Menu {
-  constructor(orderName, customerName, orderTotal) {
+  constructor(orderName, customerName, orderTotal, orderId) {
     super();
     this.orderList = [];
     this.orderFormat = {
+      id: orderId,
       name: orderName,
       customer_name: customerName,
       menu: super.menuList,
