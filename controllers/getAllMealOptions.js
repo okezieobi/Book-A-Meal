3472ca465505deda977b7e423af557c0d84be08b
okezieobi <obiedereokezie@gmail.com>
2019-02-16 22:37:14 +0100

    [Feature #163682759] Built and tested an endpoint that retrieves all meal options

--- controllers/getAllMealOptions.js
+++ controllers/getAllMealOptions.js
@@ -1,6 +1,17 @@
+import data from '../models';
 // @ts-ignore
 import bookAMeal from './index';
 
-bookAMeal.getAllMealOptions = async () => {};
+bookAMeal.getAllMealOptions = async (req, res) => {
+  try {
+    const allMealOptions = data.meals.mealOptionList;
+    res.status(200).send({
+      message: 'Success! Meal options retrieved',
+      data: allMealOptions,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
 
 export default bookAMeal.getAllMealOptions;
---
+++ test/getAllMealOptions.js
@@ -0,0 +1,42 @@
+import {
+  expect,
+  chai,
+  chaiHttp,
+  app,
+} from './index';
+import data from '../models';
+
+chai.use(chaiHttp);
+
+describe('Test endpoint at "/v1/meals" wih GET to retrieve all meal options', () => {
+  before(async () => {
+    const testDataOne = {
+      id: data.meals.mealOptionList.length,
+      name: 'Dodo',
+      price: 100,
+    };
+    const testDataTwo = {
+      id: data.meals.mealOptionList.length,
+      name: 'Rice',
+      price: 1000,
+    };
+    const testDataThree = {
+      id: data.meals.mealOptionList.length,
+      name: 'Beans',
+      price: 10000,
+    };
+    const testDataList = [testDataOne, testDataTwo, testDataThree];
+    await data.meals.mealOptionList.push(...testDataList);
+  });
+
+  it('should get all meal options at "/v1/meals" with GET', async () => {
+    const response = await chai.request(app).get('/v1/meals');
+    const randomElement = response.body.data[Math.floor(Math.random() * response.body.data.length)];
+    expect(response).to.have.status(200);
+    expect(response.body).to.be.an('object');
+    expect(randomElement).to.have.property('id');
+    expect(randomElement).to.have.property('name');
+    expect(randomElement).to.have.property('price');
+    expect(response.body).to.have.property('message').equal('Success! Meal options retrieved');
+  });
+});
