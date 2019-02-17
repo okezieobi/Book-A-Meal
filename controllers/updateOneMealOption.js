10a13fc682327e07aeb14c3e5afbd3276369ccec
okezieobi <obiedereokezie@gmail.com>
2019-02-17 10:11:01 +0100

    [Feature #163682759] started work on endpoint to update or make a new meal option

--- controllers/updateOneMealOption.js
+++ controllers/updateOneMealOption.js
@@ -1,6 +1,32 @@
+import data from '../models';
 // @ts-ignore
 import bookAMeal from './index';
 
-bookAMeal.updateOneMealOption = async () => {};
+bookAMeal.updateOneMealOption = async (req, res) => {
+  try {
+    const {
+      mealOptionName,
+      mealOptionPrice,
+    } = req.body;
+    const id = parseInt(req.params.mealId, 10);
+    if (!mealOptionName || mealOptionName === '') {
+      res.status(400).send({
+        message: 'Fail! meal option name is required',
+      });
+    } else if (!mealOptionPrice || mealOptionPrice === '') {
+      res.status(400).send({
+        message: 'Fail! Meal option price is required',
+      });
+    } else {
+      const findMealOption = data.meals.mealOptionList.fill(mealOption => mealOption.id === id);
+      if (!findMealOption) {
+        data.meals.mealOptionData.price = data.meals.mealOptionList.length;
+        const newMealOption = await data.meals.mealFormat(req.body);
+      }
+    }
+  } catch (err) {
+    throw err;
+  }
+};
 
 export default bookAMeal.updateOneMealOption;
