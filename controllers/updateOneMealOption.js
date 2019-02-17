0b97749d683abdddc2a1f4a985b7f9b8114450d8
okezieobi <obiedereokezie@gmail.com>
2019-02-17 20:41:32 +0100

    [Feature #163682759] Built and tested an endpoint that updates or creates a meal option

--- controllers/updateOneMealOption.js
+++ controllers/updateOneMealOption.js
@@ -8,20 +8,41 @@ bookAMeal.updateOneMealOption = async (req, res) => {
       mealOptionName,
       mealOptionPrice,
     } = req.body;
-    const id = parseInt(req.params.mealId, 10);
+    const id = parseInt(req.params.id, 10);
     if (!mealOptionName || mealOptionName === '') {
       res.status(400).send({
         message: 'Fail! meal option name is required',
+      });
+    } else if ((/^[A-Za-z]+$/).test(mealOptionName) === false) {
+      res.status(400).send({
+        message: 'Fail! Meal option name must be letters',
       });
     } else if (!mealOptionPrice || mealOptionPrice === '') {
       res.status(400).send({
         message: 'Fail! Meal option price is required',
       });
+    } else if ((/^[0-9]+$/).test(mealOptionPrice) === false) {
+      res.status(400).send({
+        message: 'Fail! Meal option price must be numbers',
+      });
     } else {
-      const findMealOption = data.meals.mealOptionList.fill(mealOption => mealOption.id === id);
+      const findMealOption = data.meals.mealOptionList.find(mealOption => mealOption.id === id);
       if (!findMealOption) {
-        data.meals.mealOptionData.price = data.meals.mealOptionList.length;
-        const newMealOption = await data.meals.mealFormat(req.body);
+        req.body.mealId = data.meals.mealOptionList.length;
+        const createdMealOption = await data.meals.mealFormat(req.body);
+        await data.meals.mealOptionList.push(createdMealOption);
+        res.status(201).send({
+          message: 'Meal option not found! Meal option successfully created',
+          data: createdMealOption,
+        });
+      } else {
+        req.body.mealId = findMealOption.id;
+        const updatedMealOption = await data.meals.mealFormat(req.body);
+        await data.meals.mealOptionList.splice(findMealOption.id, 1, updatedMealOption);
+        res.status(200).send({
+          message: 'Meal option found! Meal option successfully updated',
+          data: updatedMealOption,
+        });
       }
     }
   } catch (err) {
