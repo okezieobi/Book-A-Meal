c857f9353d720aefc431775de8ea5ea66e32a7fb
okezieobi <obiedereokezie@gmail.com>
2019-02-19 17:22:04 +0100

    [Feature #163682836] Built and tested api that gets all menus

--- controllers/getMenu.js
+++ controllers/getMenu.js
@@ -1,6 +1,17 @@
+import data from '../models';
 // @ts-ignore
 import bookAMeal from './index';
 
-bookAMeal.getMenu = async () => {};
+bookAMeal.getMenu = async (req, res) => {
+  try {
+    const allMenus = data.menus.menuList;
+    res.status(200).send({
+      message: 'Success! Menu list retrieved',
+      data: allMenus,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
 
 export default bookAMeal.getMenu;
---
+++ test/getMenu.js
@@ -0,0 +1,68 @@
+import {
+  expect,
+  chai,
+  chaiHttp,
+  app,
+} from './index';
+import data from '../models';
+
+chai.use(chaiHttp);
+
+describe('Test endpoint at "/v1/menus" to get all menus with GET', () => {
+  before(async () => {
+    const testDataOne = {
+      id: data.meals.mealOptionList.length,
+      name: 'Dodo',
+      price: 100,
+    };
+    const testDataTwo = {
+      id: data.meals.mealOptionList.length,
+      name: 'Rice',
+      price: 1000,
+    };
+    const testDataThree = {
+      id: data.meals.mealOptionList.length,
+      name: 'Beans',
+      price: 10000,
+    };
+
+    const testDataFour = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Launch',
+      menuOptions: 'Rice Dodo',
+    };
+
+    const testDataFive = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Dinner',
+      menuOptions: 'Rice Beans',
+    };
+    const testDataSix = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Breakfast',
+      menuOptions: 'Beans Dodo',
+    };
+
+    const testDataList = [testDataOne, testDataTwo, testDataThree];
+    await data.meals.mealOptionList.push(...testDataList);
+    const addOne = await data.menus.menuFormat(testDataFour);
+    const addTwo = await data.menus.menuFormat(testDataFive);
+    const addThree = await data.menus.menuFormat(testDataSix);
+    const testDataListTwo = [addOne, addTwo, addThree];
+    await data.menus.menuList.push(...testDataListTwo);
+  });
+
+  it('should get all menus at "/v1/menus" with GET', async () => {
+    const response = await chai.request(app).get('/v1/menus');
+    const randomElement = response.body.data[Math.floor(Math.random() * response.body.data.length)];
+    expect(response).to.have.status(200);
+    expect(response).to.be.an('object');
+    expect(response.body).to.have.property('data');
+    expect(response.body).to.have.property('message').equal('Success! Menu list retrieved');
+    expect(randomElement).to.have.property('id');
+    expect(randomElement).to.have.property('name');
+    expect(randomElement).to.have.property('total');
+    expect(randomElement).to.have.property('mealOptions');
+    expect(randomElement).to.have.property('date');
+  });
+});
