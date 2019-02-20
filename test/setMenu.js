a8f042bb8bcf652d6ce40795222a97c6efe5a7ac
okezieobi <obiedereokezie@gmail.com>
2019-02-20 09:55:27 +0100

    [Feature #163682797] Refracted test file

--- test/setMenu.js
+++ test/setMenu.js
@@ -36,11 +36,12 @@ describe('Test endpoint at "/v1/menus" to set menus with POST', () => {
     };
     const response = await chai.request(app).post('/v1/menus').send(testData);
     expect(response).to.have.status(201);
-    expect(response.body).to.be.an('object');
+    expect(response).to.be.an('object');
+    expect(response.body).to.have.property('data');
+    expect(response.body).to.have.property('message').equal('Success! Menu created');
     expect(response.body.data).to.have.property('id');
     expect(response.body.data).to.have.property('name').equal(testData.menuName);
     expect(response.body.data).to.have.property('total');
-    expect(response.body).to.have.property('message').equal('Success! Menu created');
   });
 
   it('should not create a menu at "/v1/menus" with POST if menu name in request does not exist', async () => {
