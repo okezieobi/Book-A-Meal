8d9bfc1525d4b65bae59938545469e88ed822899
okezieobi <obiedereokezie@gmail.com>
2019-02-20 10:29:45 +0100

    [Feature #163682759] Refacted test file

--- test/getAllMealOptions.js
+++ test/getAllMealOptions.js
@@ -34,9 +34,10 @@ describe('Test endpoint at "/v1/meals" wih GET to retrieve all meal options', ()
     const randomElement = response.body.data[Math.floor(Math.random() * response.body.data.length)];
     expect(response).to.have.status(200);
     expect(response.body).to.be.an('object');
+    expect(response.body).to.have.property('data');
+    expect(response.body).to.have.property('message').equal('Success! Meal options retrieved');
     expect(randomElement).to.have.property('id');
     expect(randomElement).to.have.property('name');
     expect(randomElement).to.have.property('price');
-    expect(response.body).to.have.property('message').equal('Success! Meal options retrieved');
   });
 });
