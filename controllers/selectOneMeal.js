374378565fe0a8a1490e86b96cdd62824c9f7aea
okezieobi <obiedereokezie@gmail.com>
2019-02-21 12:06:56 +0100

    [Feature #163682863] Started worked on endpoint

--- controllers/selectOneMeal.js
+++ controllers/selectOneMeal.js
@@ -1,6 +1,15 @@
 // @ts-ignore
 import bookAMeal from './index';
 
-bookAMeal.selectOneMeal = async () => {};
+bookAMeal.selectOneMeal = async (req, res) => {
+  try {
+    const {
+      orderName,
+      customerName,
+    }
+  } catch (err) {
+    throw err;
+  }
+};
 
 export default bookAMeal.selectOneMeal;
