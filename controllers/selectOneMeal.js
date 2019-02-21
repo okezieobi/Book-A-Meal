fe4dc68c76d4a3a5bd2b3bc0f7a46ba4e4e198e6
okezieobi <obiedereokezie@gmail.com>
2019-02-21 21:47:42 +0100

    [Feature #163682863] Built and tested endpoints that selects a menu option

--- controllers/selectOneMeal.js
+++ controllers/selectOneMeal.js
@@ -10,13 +10,13 @@ bookAMeal.selectOneMeal = async (req, res) => {
     } = req.body;
     if (!customerName || customerName === '') {
       res.status(400).send({
-        message: 'Fail! Menu name is required',
+        message: 'Fail! Customer name is required',
       });
     } else if ((/^[A-Za-z]+$/).test(customerName) === false) {
       res.status(400).send({
-        message: 'Fail! Menu name must be letters with no spacing',
+        message: 'Fail! Customer name must be letters with no spacing',
       });
-    } else if (!menuList) {
+    } else if (!menuList || menuList === '') {
       res.status(400).send({
         message: 'Fail! Menu options are required',
       });
