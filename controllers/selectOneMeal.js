bbb0450917fc288617d7c7ab72350d1bb667da3f
okezieobi <obiedereokezie@gmail.com>
2019-02-21 20:50:54 +0100

    [Feature #163682863] wrote test and endpoint to select a menu option

--- controllers/selectOneMeal.js
+++ controllers/selectOneMeal.js
@@ -1,11 +1,37 @@
+import data from '../models';
 // @ts-ignore
 import bookAMeal from './index';
 
 bookAMeal.selectOneMeal = async (req, res) => {
   try {
     const {
-      orderName,
       customerName,
+      menuList,
+    } = req.body;
+    if (!customerName || customerName === '') {
+      res.status(400).send({
+        message: 'Fail! Menu name is required',
+      });
+    } else if ((/^[A-Za-z]+$/).test(customerName) === false) {
+      res.status(400).send({
+        message: 'Fail! Menu name must be letters with no spacing',
+      });
+    } else if (!menuList) {
+      res.status(400).send({
+        message: 'Fail! Menu options are required',
+      });
+    } else if ((/^[A-Za-z\s]+$/).test(menuList) === false) {
+      res.status(400).send({
+        message: 'Fail! Menu options must be letters and seperated by spaces',
+      });
+    } else {
+      req.body.orderId = data.orders.orderList.length;
+      const selectedMeal = await data.orders.orderFormat(req.body);
+      await data.orders.orderList.push(selectedMeal);
+      res.status(201).send({
+        message: 'Success! Menu selected and order made',
+        data: selectedMeal,
+      });
     }
   } catch (err) {
     throw err;
--- test/selectOneMeal.js
+++ test/selectOneMeal.js
@@ -51,4 +51,17 @@ describe('Test endpoint at "/v1/orders" to select meal option with POST', () => 
     const testDataListTwo = [addOne, addTwo, addThree];
     await data.menus.menuList.push(...testDataListTwo);
   });
+
+  it('should select at least one menu option at "/v1/orders" with POST', async () => {
+    const testData = {
+      customerName: 'Frank',
+      menuList: 'BreakFast Dinner',
+    };
+    const response = await chai.request(app).post('/v1/orders').send(testData);
+    expect(response).to.have.status(201);
+    expect(response).to.be.an('object');
+    expect(response.body).to.have.property('data');
+    expect(response.body).to.have.property('message').equal('Success! Menu selected and order made');
+    expect(response.body.data).to.have.property('id');
+  });
 });
