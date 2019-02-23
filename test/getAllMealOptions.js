f75b4fdadb0ef23a337700ae3eb84b4e621cbad0
okezieobi <obiedereokezie@gmail.com>
2019-02-23 12:58:54 +0100

    [Feature #163682759] Updated test file for more coverage

--- test/getAllMealOptions.js
+++ test/getAllMealOptions.js
@@ -25,8 +25,54 @@ describe('Test endpoint at "/v1/meals" wih GET to retrieve all meal options', ()
       name: 'Beans',
       price: 10000,
     };
+
+    const testDataFour = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Launch',
+      menuOptions: 'Rice Dodo',
+    };
+
+    const testDataFive = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Dinner',
+      menuOptions: 'Rice Beans',
+    };
+    const testDataSix = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Breakfast',
+      menuOptions: 'Beans Dodo',
+    };
+
+    const testDataSeven = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Okezie',
+      menuList: 'Breakfast Lunch',
+    };
+
+    const testDataEight = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Frank',
+      menuList: 'Breakfast Dinner',
+    };
+
+    const testDataNine = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Obiedere',
+      menuList: 'Lunch Dinner',
+    };
+
     const testDataList = [testDataOne, testDataTwo, testDataThree];
     await data.meals.mealOptionList.push(...testDataList);
+    const addOne = await data.menus.menuFormat(testDataFour);
+    const addTwo = await data.menus.menuFormat(testDataFive);
+    const addThree = await data.menus.menuFormat(testDataSix);
+    const testDataListTwo = [addOne, addTwo, addThree];
+    await data.menus.menuList.push(...testDataListTwo);
+    const addFour = await data.orders.orderFormat(testDataSeven);
+    const addFive = await data.orders.orderFormat(testDataEight);
+    const addSix = await data.orders.orderFormat(testDataNine);
+    const testDataListThree = [addFour, addFive, addSix];
+    await data.orders.orderList.push(...testDataListThree);
   });
 
   it('should get all meal options at "/v1/meals" with GET', async () => {
