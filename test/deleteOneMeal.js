5c53ab43fecf573fa40b9e8edaa3af6278410a80
okezieobi <obiedereokezie@gmail.com>
2019-02-23 12:44:28 +0100

    [Feature #163682759] Updated test file for more coverage

--- test/deleteOneMeal.js
+++ test/deleteOneMeal.js
@@ -10,15 +10,71 @@ chai.use(chaiHttp);
 
 describe('Test endpoint at "/v1/meals/:id" to delete a meals option with DELETE', () => {
   let testId = 0;
-
   before(async () => {
-    const testData = {
-      id: 0,
+    const testDataOne = {
+      id: data.meals.mealOptionList.length,
       name: 'Dodo',
       price: 100,
     };
-    await data.meals.mealOptionList.push(testData);
-    testId += testData.id;
+    const testDataTwo = {
+      id: data.meals.mealOptionList.length,
+      name: 'Rice',
+      price: 1000,
+    };
+    const testDataThree = {
+      id: data.meals.mealOptionList.length,
+      name: 'Beans',
+      price: 10000,
+    };
+
+    const testDataFour = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Launch',
+      menuOptions: 'Rice Dodo',
+    };
+
+    const testDataFive = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Dinner',
+      menuOptions: 'Rice Beans',
+    };
+    const testDataSix = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Breakfast',
+      menuOptions: 'Beans Dodo',
+    };
+
+    const testDataSeven = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Okezie',
+      menuList: 'Breakfast Lunch',
+    };
+
+    const testDataEight = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Frank',
+      menuList: 'Breakfast Dinner',
+    };
+
+    const testDataNine = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Obiedere',
+      menuList: 'Lunch Dinner',
+    };
+
+    const testDataList = [testDataOne, testDataTwo, testDataThree];
+    await data.meals.mealOptionList.push(...testDataList);
+    const addOne = await data.menus.menuFormat(testDataFour);
+    const addTwo = await data.menus.menuFormat(testDataFive);
+    const addThree = await data.menus.menuFormat(testDataSix);
+    const testDataListTwo = [addOne, addTwo, addThree];
+    await data.menus.menuList.push(...testDataListTwo);
+    const addFour = await data.orders.orderFormat(testDataSeven);
+    const addFive = await data.orders.orderFormat(testDataEight);
+    const addSix = await data.orders.orderFormat(testDataNine);
+    const testDataListThree = [addFour, addFive, addSix];
+    await data.orders.orderList.push(...testDataListThree);
+    testId += testDataOne.id;
   });
 
   it('should delete a meal option at "/v1/meals/:id" with DELETE if meals option exists', async () => {
