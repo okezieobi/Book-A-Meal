2b5de4d7542584671530f5f5b7ce3d38d01bde02
okezieobi <obiedereokezie@gmail.com>
2019-02-23 12:33:18 +0100

    [Feature #163682759] updated test file

--- test/addOneMeal.js
+++ test/addOneMeal.js
@@ -5,9 +5,77 @@ import {
   app,
 } from './index';
 
+import data from '../models';
+
 chai.use(chaiHttp);
 
 describe('Test endpoint at "/v1/meals" to create a meal option with POST', () => {
+  before(async () => {
+    const testDataOne = {
+      id: data.meals.mealOptionList.length,
+      name: 'Dodo',
+      price: 100,
+    };
+    const testDataTwo = {
+      id: data.meals.mealOptionList.length,
+      name: 'Rice',
+      price: 1000,
+    };
+    const testDataThree = {
+      id: data.meals.mealOptionList.length,
+      name: 'Beans',
+      price: 10000,
+    };
+
+    const testDataFour = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Launch',
+      menuOptions: 'Rice Dodo',
+    };
+
+    const testDataFive = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Dinner',
+      menuOptions: 'Rice Beans',
+    };
+    const testDataSix = {
+      menuId: data.menus.menuList.length,
+      menuName: 'Breakfast',
+      menuOptions: 'Beans Dodo',
+    };
+
+    const testDataSeven = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Okezie',
+      menuList: 'Breakfast Lunch',
+    };
+
+    const testDataEight = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Frank',
+      menuList: 'Breakfast Dinner',
+    };
+
+    const testDataNine = {
+      orderId: data.orders.orderList.length,
+      customerName: 'Obiedere',
+      menuList: 'Lunch Dinner',
+    };
+
+    const testDataList = [testDataOne, testDataTwo, testDataThree];
+    await data.meals.mealOptionList.push(...testDataList);
+    const addOne = await data.menus.menuFormat(testDataFour);
+    const addTwo = await data.menus.menuFormat(testDataFive);
+    const addThree = await data.menus.menuFormat(testDataSix);
+    const testDataListTwo = [addOne, addTwo, addThree];
+    await data.menus.menuList.push(...testDataListTwo);
+    const addFour = await data.orders.orderFormat(testDataSeven);
+    const addFive = await data.orders.orderFormat(testDataEight);
+    const addSix = await data.orders.orderFormat(testDataNine);
+    const testDataListThree = [addFour, addFive, addSix];
+    await data.orders.orderList.push(...testDataListThree);
+  });
+
   it('should create a meal option at "/v1/meal" with post if all request data are valid', async () => {
     const testData = {
       mealOptionName: 'Dodo',
