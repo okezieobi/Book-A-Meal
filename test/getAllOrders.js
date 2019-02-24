15899bd741a75ae7bc94ca0b05ac22dc588499bf
okezieobi <obiedereokezie@gmail.com>
2019-02-24 10:28:35 +0100

    [Feature #163682903] Updated test and test index file

--- test/getAllOrders.js
+++ test/getAllOrders.js
@@ -3,76 +3,16 @@ import {
   chai,
   chaiHttp,
   app,
+  dataSetup,
 } from './index';
-import data from '../models';
 
 chai.use(chaiHttp);
 
 describe('Test endpoint at "/v1/orders" to get all orders with GET', () => {
   before(async () => {
-    const testDataOne = {
-      id: data.meals.mealOptionList.length,
-      name: 'Dodo',
-      price: 100,
-    };
-    const testDataTwo = {
-      id: data.meals.mealOptionList.length,
-      name: 'Rice',
-      price: 1000,
-    };
-    const testDataThree = {
-      id: data.meals.mealOptionList.length,
-      name: 'Beans',
-      price: 10000,
-    };
-
-    const testDataFour = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Launch',
-      menuOptions: 'Rice Dodo',
-    };
-
-    const testDataFive = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Dinner',
-      menuOptions: 'Rice Beans',
-    };
-    const testDataSix = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Breakfast',
-      menuOptions: 'Beans Dodo',
-    };
-
-    const testDataSeven = {
-      orderId: data.orders.orderList.length,
-      customerName: 'Okezie',
-      menuList: 'Breakfast Lunch',
-    };
-
-    const testDataEight = {
-      orderId: data.orders.orderList.length,
-      customerName: 'Frank',
-      menuList: 'Breakfast Dinner',
-    };
-
-    const testDataNine = {
-      orderId: data.orders.orderList.length,
-      customerName: 'Obiedere',
-      menuList: 'Lunch Dinner',
-    };
-
-    const testDataList = [testDataOne, testDataTwo, testDataThree];
-    await data.meals.mealOptionList.push(...testDataList);
-    const addOne = await data.menus.menuFormat(testDataFour);
-    const addTwo = await data.menus.menuFormat(testDataFive);
-    const addThree = await data.menus.menuFormat(testDataSix);
-    const testDataListTwo = [addOne, addTwo, addThree];
-    await data.menus.menuList.push(...testDataListTwo);
-    const addFour = await data.orders.orderFormat(testDataSeven);
-    const addFive = await data.orders.orderFormat(testDataEight);
-    const addSix = await data.orders.orderFormat(testDataNine);
-    const testDataListThree = [addFour, addFive, addSix];
-    await data.orders.orderList.push(...testDataListThree);
+    dataSetup.meals();
+    dataSetup.menus();
+    dataSetup.orders();
   });
 
   it('should get all orders at "/v1/orders" with GET', async () => {
