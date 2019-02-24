c78df71a27e3f1161642131b62680dd4febd1e11
okezieobi <obiedereokezie@gmail.com>
2019-02-24 10:23:33 +0100

    [Feature #163682836] Updated test and test index file

--- test/getMenu.js
+++ test/getMenu.js
@@ -3,53 +3,17 @@ import {
   chai,
   chaiHttp,
   app,
+  dataSetup,
 } from './index';
-import data from '../models';
+
 
 chai.use(chaiHttp);
 
 describe('Test endpoint at "/v1/menus" to get all menus with GET', () => {
   before(async () => {
-    const testDataOne = {
-      id: data.meals.mealOptionList.length,
-      name: 'Dodo',
-      price: 100,
-    };
-    const testDataTwo = {
-      id: data.meals.mealOptionList.length,
-      name: 'Rice',
-      price: 1000,
-    };
-    const testDataThree = {
-      id: data.meals.mealOptionList.length,
-      name: 'Beans',
-      price: 10000,
-    };
-
-    const testDataFour = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Launch',
-      menuOptions: 'Rice Dodo',
-    };
-
-    const testDataFive = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Dinner',
-      menuOptions: 'Rice Beans',
-    };
-    const testDataSix = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Breakfast',
-      menuOptions: 'Beans Dodo',
-    };
-
-    const testDataList = [testDataOne, testDataTwo, testDataThree];
-    await data.meals.mealOptionList.push(...testDataList);
-    const addOne = await data.menus.menuFormat(testDataFour);
-    const addTwo = await data.menus.menuFormat(testDataFive);
-    const addThree = await data.menus.menuFormat(testDataSix);
-    const testDataListTwo = [addOne, addTwo, addThree];
-    await data.menus.menuList.push(...testDataListTwo);
+    dataSetup.meals();
+    dataSetup.menus();
+    dataSetup.orders();
   });
 
   it('should get all menus at "/v1/menus" with GET', async () => {
