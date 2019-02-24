730c5801e2f59b48bf6dec1f3389291d2e006411
okezieobi <obiedereokezie@gmail.com>
2019-02-24 06:55:51 +0100

    [Feature #163684425] Updated test index file

--- test/updateOneOrder.js
+++ test/updateOneOrder.js
@@ -3,78 +3,21 @@ import {
   chai,
   chaiHttp,
   app,
+  mealSetup,
+  menuSetup,
+  orderSetup,
 } from './index';
-import data from '../models';
+
 
 chai.use(chaiHttp);
 
 describe('Test endpoint at "/v1/orders/:id" to update or create a menu order', () => {
   let testOrderId = 0;
-  before(async () => {
-    const testDataOne = {
-      id: data.meals.mealOptionList.length,
-      name: 'Dodo',
-      price: 100,
-    };
-    const testDataTwo = {
-      id: data.meals.mealOptionList.length,
-      name: 'Rice',
-      price: 1000,
-    };
-    const testDataThree = {
-      id: data.meals.mealOptionList.length,
-      name: 'Beans',
-      price: 10000,
-    };
-
-    const testDataFour = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Launch',
-      menuOptions: 'Rice Dodo',
-    };
-
-    const testDataFive = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Dinner',
-      menuOptions: 'Rice Beans',
-    };
-    const testDataSix = {
-      menuId: data.menus.menuList.length,
-      menuName: 'Breakfast',
-      menuOptions: 'Beans Dodo',
-    };
-
-    const testDataSeven = {
-      orderId: data.orders.orderList.length,
-      customerName: 'Okezie',
-      menuList: 'Breakfast Lunch',
-    };
-
-    const testDataEight = {
-      orderId: data.orders.orderList.length,
-      customerName: 'Frank',
-      menuList: 'Breakfast Dinner',
-    };
-
-    const testDataNine = {
-      orderId: data.orders.orderList.length,
-      customerName: 'Obiedere',
-      menuList: 'Lunch Dinner',
-    };
-
-    const testDataList = [testDataOne, testDataTwo, testDataThree];
-    await data.meals.mealOptionList.push(...testDataList);
-    const addOne = await data.menus.menuFormat(testDataFour);
-    const addTwo = await data.menus.menuFormat(testDataFive);
-    const addThree = await data.menus.menuFormat(testDataSix);
-    const testDataListTwo = [addOne, addTwo, addThree];
-    await data.menus.menuList.push(...testDataListTwo);
-    const addFour = await data.orders.orderFormat(testDataSeven);
-    const addFive = await data.orders.orderFormat(testDataEight);
-    const addSix = await data.orders.orderFormat(testDataNine);
-    const testDataListThree = [addFour, addFive, addSix];
-    await data.orders.orderList.push(...testDataListThree);
-    testOrderId += addFive.id;
+  before(() => {
+    mealSetup.setUpMealData();
+    menuSetup.setupMenuData();
+    orderSetup.setupOrderData();
+    testOrderId += orderSetup.secondTestId;
   });
 
   it('should update a menu order at "/v1/orders/:id" with PUT if all data in request are valid', async () => {
