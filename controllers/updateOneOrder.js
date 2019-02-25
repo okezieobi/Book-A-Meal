98fc2ea40d21ac52be9ba74cdea284812dd8b609
okezieobi <obiedereokezie@gmail.com>
2019-02-25 10:50:58 +0100

    [Feature #163684425] Updated controller and added services

--- controllers/updateOneOrder.js
+++ controllers/updateOneOrder.js
@@ -1,28 +1,26 @@
 import data from '../models';
-import {
-  orderServices,
-} from '../services';
+import services from '../services';
 // @ts-ignore
 import bookAMeal from './index';
 
 bookAMeal.updateOneOrder = async (req, res) => {
   try {
     if (!req.body.customerName || req.body.customerName === '') {
-      res.status(400).send(orderServices.orderErrOne);
+      res.status(400).send(services.requiredNameErr('Customer name'));
     } else if ((/^[A-Za-z]+$/).test(req.body.customerName) === false) {
-      res.status(400).send(orderServices.orderErrTwo);
+      res.status(400).send(services.mustBeLettersErr('Customer name'));
     } else if (!req.body.menuList || req.body.menuList === '') {
-      res.status(400).send(orderServices.orderErrThree);
+      res.status(400).send(services.requiredNameErr('Menu list'));
     } else if ((/^[A-Za-z\s]+$/).test(req.body.menuList) === false) {
-      res.status(400).send(orderServices.orderErrFour);
+      res.status(400).send(services.stringToArrayErr('Menu list'));
     } else {
-      const findOrder = orderServices.findOne(req.params, data.orders.orderList);
+      const findOrder = services.findOne(req.params, data.orders.orderList);
       if (!findOrder) {
         req.body.orderId = data.orders.orderList.length;
-        orderServices.createOne(res, data.orders.orderList, data.orders.orderFormat(req.body), 'Menu order not found, menu order successfully created');
+        services.createOne(res, data.orders.orderList, data.orders.orderFormat(req.body), 'Menu order not found, menu order successfully created');
       } else {
         req.body.orderId = findOrder.id;
-        orderServices.updateOne(res, data.orders.orderList, data.orders.orderFormat(req.body), 'Menu order found, menu order successfully updated');
+        services.updateOne(res, data.orders.orderList, data.orders.orderFormat(req.body), 'Menu order found, menu order successfully updated');
       }
     }
   } catch (err) {
