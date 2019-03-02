ca12f00af00643a8e12704851406dfecc8b456d7
okezieobi <obiedereokezie@gmail.com>
2019-03-02 22:06:10 +0100

    [Feature #163682759] Built and tested endpoint that updates or creates a meal option

--- controllers/updateMeal.js
+++ controllers/updateMeal.js
@@ -1,6 +1,19 @@
+import data from '../models';
+import services from '../services';
 // @ts-ignore
 import bookAMeal from './index';
 
-bookAMeal.updateOneMealOption = async () => {};
+bookAMeal.updateOneMealOption = async (req, res) => {
+  if ((req.body.mealOptionName && req.body.mealOptionPrice && (/^[A-Za-z]+$/).test(req.body.mealOptionName) && (/^[0-9]+$/).test(req.body.mealOptionPrice)) === true && (req.body.mealOptionName && req.body.mealOptionPrice) !== '') {
+    const findMeal = services.findOne(req.params, data.meals.mealOptionList);
+    if (findMeal) {
+      services.updateOne(res, data.meals.mealOptionList, data.meals.mealFormat(req.body), 'Meal option found! Meal option successfully updated', findMeal.id);
+      return;
+    }
+    services.createOne(res, data.meals.mealOptionList, data.meals.mealFormat(req.body), 'Meal option not found! Meal option successfully created', req.body.mealId);
+    return;
+  }
+  services.processErr(req.body.mealOptionName, req.body.mealOptionPrice, 'Meal option name', 'Meal option price', services.mustBeNumbersErr('Meal option price'), res);
+};
 
 export default bookAMeal.updateOneMealOption;
--- dist/controllers/updateMeal.js
+++ dist/controllers/updateMeal.js
@@ -5,6 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _models = _interopRequireDefault(require("../models"));
+
+var _services = _interopRequireDefault(require("../services"));
+
 var _index = _interopRequireDefault(require("./index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -15,19 +19,52 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 _index.default.updateOneMealOption =
 /*#__PURE__*/
-_asyncToGenerator(
-/*#__PURE__*/
-regeneratorRuntime.mark(function _callee() {
-  return regeneratorRuntime.wrap(function _callee$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-        case "end":
-          return _context.stop();
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  regeneratorRuntime.mark(function _callee(req, res) {
+    var findMeal;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            if (!((req.body.mealOptionName && req.body.mealOptionPrice && /^[A-Za-z]+$/.test(req.body.mealOptionName) && /^[0-9]+$/.test(req.body.mealOptionPrice)) === true && (req.body.mealOptionName && req.body.mealOptionPrice) !== '')) {
+              _context.next = 7;
+              break;
+            }
+
+            findMeal = _services.default.findOne(req.params, _models.default.meals.mealOptionList);
+
+            if (!findMeal) {
+              _context.next = 5;
+              break;
+            }
+
+            _services.default.updateOne(res, _models.default.meals.mealOptionList, _models.default.meals.mealFormat(req.body), 'Meal option found! Meal option successfully updated', findMeal.id);
+
+            return _context.abrupt("return");
+
+          case 5:
+            _services.default.createOne(res, _models.default.meals.mealOptionList, _models.default.meals.mealFormat(req.body), 'Meal option not found! Meal option successfully created', req.body.mealId);
+
+            return _context.abrupt("return");
+
+          case 7:
+            _services.default.processErr(req.body.mealOptionName, req.body.mealOptionPrice, 'Meal option name', 'Meal option price', _services.default.mustBeNumbersErr('Meal option price'), res);
+
+          case 8:
+          case "end":
+            return _context.stop();
+        }
       }
-    }
-  }, _callee);
-}));
+    }, _callee);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
 var _default = _index.default.updateOneMealOption;
 exports.default = _default;
 //# sourceMappingURL=updateMeal.js.map
