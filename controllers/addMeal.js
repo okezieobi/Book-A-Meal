6ae04943f02aa02eb243e58eafbcdcbd02a97815
okezieobi <obiedereokezie@gmail.com>
2019-03-03 10:22:13 +0100

    [Feature #163682759] Initial commits for branch

--- controllers/addMeal.js
+++ controllers/addMeal.js
@@ -1,6 +1,14 @@
+import data from '../models';
+import services from '../services';
 // @ts-ignore
 import bookAMeal from './index';
 
-bookAMeal.addOneMealOption = async () => {};
+bookAMeal.addOneMealOption = async (req, res) => {
+  const testMeal = (req.body.mealOptionName && req.body.mealOPtionPrice && (/^[A-Za-z]+$/).test(req.body.mealOptionName) && (/^[0-9]+$/).test(req.body.mealOptionPrice)) && (req.body.mealOptionName && req.body.mealOPtionPrice) !== '';
+  if (testMeal) {
+    req.body.mealId = data.meals.mealOptionList.length;
+    services.createOne(res, data.meals.mealOptionList, data.meals.mealFormat(req.body), 'Success! Meal option created');
+  }
+};
 
 export default bookAMeal.addOneMealOption;
