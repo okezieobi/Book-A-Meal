b1d2102783ec74c41aa531f7398a797a847d5ddb
okezieobi <obiedereokezie@gmail.com>
2019-03-03 15:02:44 +0100

    [Feature ft-deleteMeal-#163682759] Built and tested an endpoint that deletes a meal option

--- dist/controllers/deleteMeal.js
+++ dist/controllers/deleteMeal.js
@@ -5,6 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _models = _interopRequireDefault(require("../models"));
+
+var _services = _interopRequireDefault(require("../services"));
+
 var _index = _interopRequireDefault(require("./index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -15,19 +19,44 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 _index.default.deleteOneMealOption =
 /*#__PURE__*/
-_asyncToGenerator(
-/*#__PURE__*/
-regeneratorRuntime.mark(function _callee() {
-  return regeneratorRuntime.wrap(function _callee$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-        case "end":
-          return _context.stop();
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  regeneratorRuntime.mark(function _callee(req, res) {
+    var findMeal;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            findMeal = _services.default.findOne(req.params, _models.default.meals.mealOptionList);
+
+            if (!findMeal) {
+              _context.next = 4;
+              break;
+            }
+
+            _services.default.deleteOne(res, _models.default.meals.mealOptionList, findMeal.id);
+
+            return _context.abrupt("return");
+
+          case 4:
+            res.status(404).send({
+              message: 'Fail! Meal option not found'
+            });
+
+          case 5:
+          case "end":
+            return _context.stop();
+        }
       }
-    }
-  }, _callee);
-}));
+    }, _callee);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
 var _default = _index.default.deleteOneMealOption;
 exports.default = _default;
 //# sourceMappingURL=deleteMeal.js.map
