ff4a11155d27d386f6121abe6586e9fd4d5033fe
okezieobi <obiedereokezie@gmail.com>
2019-03-03 10:43:29 +0100

    [Feature #163682759] Second commit to branch

--- controllers/addMeal.js
+++ controllers/addMeal.js
@@ -4,11 +4,13 @@ import services from '../services';
 import bookAMeal from './index';
 
 bookAMeal.addOneMealOption = async (req, res) => {
-  const testMeal = (req.body.mealOptionName && req.body.mealOPtionPrice && (/^[A-Za-z]+$/).test(req.body.mealOptionName) && (/^[0-9]+$/).test(req.body.mealOptionPrice)) && (req.body.mealOptionName && req.body.mealOPtionPrice) !== '';
+  const testMeal = (req.body.mealOptionName && req.body.mealOptionPrice && (/^[A-Za-z]+$/).test(req.body.mealOptionName) && (/^[0-9]+$/).test(req.body.mealOptionPrice)) && (req.body.mealOptionName && req.body.mealOptionPrice) !== '';
   if (testMeal) {
     req.body.mealId = data.meals.mealOptionList.length;
     services.createOne(res, data.meals.mealOptionList, data.meals.mealFormat(req.body), 'Success! Meal option created');
+    return;
   }
+  services.processErr(req.body.mealOptionName, req.body.mealOptionPrice, 'Meal option name', 'Meal option price', services.mustBeNumbersErr('Meal option price'), res);
 };
 
 export default bookAMeal.addOneMealOption;
